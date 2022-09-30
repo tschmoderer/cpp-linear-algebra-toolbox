7427c7c09e970ed1b0a3d9867d99c4501b8f4f46
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-09-30 12:18:11 +0200

    try to dorce gh pages deploy

--- src/diagonalMatrix.cpp
+++ src/diagonalMatrix.cpp
@@ -1,3 +1,12 @@
+/**
+* @file diagonalMatrix.cpp
+* @author T. Schmoderer (iathena@mailo.com)
+* @version 0.1.0
+* @date 2022-09-30
+* @copyright Copyright (c) 2022. All rights reserved. This project is released under the GNU GENERAL PUBLIC LICENSE.
+* @brief Implementation file for Diagonal Matrix class
+*/
+
 #include "../include/diagonalMatrix.h"
 
 double tr(const DiagonalMatrix & D) {
