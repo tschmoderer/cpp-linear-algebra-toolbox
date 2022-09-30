0f319b3186463d54d16485c49bd78d8c8d9f884d
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-09-30 10:31:08 +0200

    Add configuration files with cmake to generate docs

--- include/diagonalMatrix.h
+++ include/diagonalMatrix.h
@@ -1,3 +1,12 @@
+/**
+* @file diagonalMatrix.h
+* @author T. Schmoderer (iathena@mailo.com)
+* @version 0.1.0
+* @date 2022-09-30
+* @copyright Copyright (c) 2022. All rights reserved. This project is released under the GNU GENERAL PUBLIC LICENSE.
+* @brief 
+*/
+
 #ifndef DIAGONAL_MATRIX_H
 #define DIAGONAL_MATRIX_H
 
