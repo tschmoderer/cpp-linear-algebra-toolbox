b243129c294f09dee8e0d5f3b72132b2a34c1231
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-10-03 18:55:41 +0200

    add logos white and B&W

--- docs/img/icons/favicon.sh
+++ docs/img/icons/favicon.sh
@@ -14,3 +14,34 @@ done
 
 convert $res $name.ico
 identify $name.ico
+
+name=cpp-linalg-logo
+name=$name-white
+svg=$name.svg
+res=
+
+for i in ${sizes[@]}; do
+    folder=$i"x"$i
+    mkdir -p $folder
+    res+=" $folder/$name.png"
+    inkscape $svg -o "$folder/$name.png" -w $i -h $i 
+done
+
+convert $res $name.ico
+identify $name.ico
+
+name=cpp-linalg-logo
+name=$name-bw
+svg=$name.svg
+sizes=(16 32 64 128 256 512)
+res=
+
+for i in ${sizes[@]}; do
+    folder=$i"x"$i
+    mkdir -p $folder
+    res+=" $folder/$name.png"
+    inkscape $svg -o "$folder/$name.png" -w $i -h $i 
+done
+
+convert $res $name.ico
+identify $name.ico
