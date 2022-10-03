345418a6e5a14421b5d7a912678c07c176ff7d3a
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-10-03 16:31:31 +0200

    add icons of project

---
+++ docs/img/icons/favicon.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name=cpp-linalg-logo
+svg=$name.svg
+sizes=(16 32 64 128 256 512)
+res=
+
+for i in ${sizes[@]}; do
+    folder=$i"x"$i
+    mkdir -p $folder
+    res+=" $folder/$name.png"
+    inkscape $svg -o "$folder/$name.png" -w $i -h $i 
+done
+
+convert $res $name.ico
+identify $name.ico
